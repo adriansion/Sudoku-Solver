5dfb8a478fade8e5faf01d45a519f7984281fa71
Adrian Sion <adriandavidsion@gmail.com>
2020-07-22 04:33:49 -0400

    Made public logging object

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,5 @@
 package main;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import solve.Backtracker;
 import structure.Grid;
 
@@ -12,10 +10,8 @@ import structure.Grid;
  */
 public class Main {
 
-	private static final Logger logger = LogManager.getLogger("Main");
-
 	public static void main(String[] args) {
-		logger.info("Starting application.");
+		Log.logger.info("Starting application.");
 		GridFileReader fileReader = new GridFileReader();
 		Verifier verifier = new Verifier();
 
@@ -23,15 +19,15 @@ public class Main {
 		Backtracker backtracker = new Backtracker();
 
 
-		logger.info("Reading and verifying grid.");
+		Log.logger.info("Reading and verifying grid.");
 		Grid unsolved = fileReader.createGrid("grid6");
 		verifier.verify(unsolved);
 
-		logger.info("Solving grid.");
+		Log.logger.info("Solving grid.");
 		Grid solved = backtracker.Solve(unsolved);
 		verifier.verify(solved);
 
-		logger.info("Grid solved.");
+		Log.logger.info("Grid solved.");
 //		System.out.println("Iterations: " + backtracker.getIterations());
 	}
 }
